a4c57919ba7350333f6106d60651f16c008b5c68
Paul Lennon <paul.lennon@gmit.ie>
2024-10-02 16:22:23 +0100

    Added a new method called Add

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,15 @@
-public class Calculator {
+public class Calculator
+{
+    public static void main(String[] args) {
+        System.out.println("Please enter your first number");
+        //scanner, read in num1 and num2
+        //switch and then call add method
+        //add(num1, num2);
+    }
+
+    public static void add(int first, int second)
+    {
+        int total = first + second;
+        System.out.println("You entered " + total);
+    }
 }
